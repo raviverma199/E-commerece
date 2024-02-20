24eb701d4e95c2150c3b00b0d214eb1b1fd95070
ravi <rv200414z@gmail.com>
2024-02-20 22:57:34 +0530

    Create app.js

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const mainRoute = require('./routes/route')
+
+
+app.use(cors( { origin: true, methods: ['GET', 'POST'] } ))
+app.use(express.json())
+app.use('/',mainRoute)
+
+
+
+
+
+app.listen(9090,()=>{
+    console.log("Server is running on port 9090")
+})
